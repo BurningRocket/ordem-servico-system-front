0fcf5085c915feef4dd5af49c2342d8cca86440e
Lucas Eleutério <leleuteiobueno@gmail.com>
2023-05-29 22:10:51 -0300

    Traduzido componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,19 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
         this.primengConfig.ripple = true;
+        this.primengConfig.setTranslation({
+            accept: 'Aceitar',
+            reject: 'Cancelar',
+            choose: 'Escolher',
+            upload: 'Enviar',
+            cancel: 'Cancelar',
+            dayNames: ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"],
+            dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+            dayNamesMin: ["D", "S", "T", "Q", "Q", "S", "S"],
+            monthNames: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"],
+            monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+            today: "Hoje",
+            weekHeader: "Sem"
+        });
     }
 }
