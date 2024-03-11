7372036b677dd5351fc212b16c6b2b2de060871d
Lucas Eleutério <leleuteiobueno@gmail.com>
2024-03-11 08:18:10 -0300

    url da lambda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    URL: 'https://pmft4routf.execute-api.us-east-1.amazonaws.com/dev/api',
+    URL: 'https://e1fpg43enb.execute-api.us-east-1.amazonaws.com/dev',
   };
